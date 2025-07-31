fea762a7a02f6a07062e5705561612625c8ad11e
Siddharth-73 <150230535+Siddharth-73@users.noreply.github.com>
2025-08-01 01:17:21 +0530

    Minor change app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         {/* 🔹 Apple iOS support */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <link rel="apple-touch-icon" href="/icons/icon-180x180.png" />
         <link rel="icon" href="/icons/icon-192x192.png" />
